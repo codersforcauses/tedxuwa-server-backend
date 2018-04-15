5837acc17c18bf8a7435fac4e5d86108bb54f08b
Anh <anhlevietexe@gmail.com>
2018-04-15 18:53:22 +0800

    regex .*

--- root/urls.py
+++ root/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     # https://medium.com/@nicholaskajoh/heres-a-dead-simple-react-django-setup-for-your-next-project-c0b0036663c6
-    re_path('', TemplateView.as_view(template_name="react_base.html")),
+    re_path('.*', TemplateView.as_view(template_name="react_base.html")),
 ]
