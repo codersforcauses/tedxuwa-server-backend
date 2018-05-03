245a69dd453fa266832161315822ce1b9fe32aaf
Anh <anhlevietexe@gmail.com>
2018-05-03 21:34:49 +0800

    Add manual deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This is a manual deployment script for ease of life. Would be much
+# better if we have an auto deployment system like git hooks
+# but I haven't been able to set it up yet
+
+# THIS ASSUME YOU'RE IN THE /
+
+# clear all screens
+pkill screen
+# front end deployment
+cd ../front-end/
+git pull
+npm build
+# back end deployment
+cd ../back-end/
+git pull
+source env/bin/activate
+python manage.py collectstatic --noinput
+# create a new detached screen and start server
+screen -d -m source env/bin/activate && python manage.py runserver 0.0.0.0:8000
