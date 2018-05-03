99f3222c442ed381196afb50b144c0ae615bc66e
Anh <anhlevietexe@gmail.com>
2018-05-03 21:39:09 +0800

    don't launch new screen

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,4 @@ git pull
 source env/bin/activate
 python manage.py collectstatic --noinput  # collect front end
 python manage.py migrate  # migrate the database
-# create a new detached screen and start server
-screen -d -m source env/bin/activate && python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000  # start the server on port 8000
