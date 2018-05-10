c32c33e126e98c42665a8798a5ff1380e272e9cf
Anh <anhlevietexe@gmail.com>
2018-05-10 11:30:50 +0800

    simplify committee endpoint test

--- tedxuwa_user/tests.py
+++ tedxuwa_user/tests.py
@@ -9,17 +9,7 @@ class TestMemberEndpoint(TestCase):
 
     def setUp(self):
         self.c = Client()
-        # 2 members in the fixture, one present one not
-        self.current_member = Member.objects.get(is_comittee=True)
-        self.past_member = Member.objects.get(is_comittee=False)
 
     def test_endpoint(self):
         re = self.c.get("/api/committee/")
         self.assertEqual(re.status_code, 200)
-        self.assertEqual(re.json()["count"], 1)
-        # making both current members should return 0 past members
-        self.past_member.is_comittee = True
-        self.past_member.save()
-        re = self.c.get("/api/committee/")
-        self.assertEqual(re.status_code, 200)
-        self.assertEqual(re.json()["count"], 2)
