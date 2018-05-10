a77f49feee8cdd19744e5fa5b1f69578e30cdbd0
Anh <anhlevietexe@gmail.com>
2018-05-10 11:34:03 +0800

    change view queryset

--- tedxuwa_user/views.py
+++ tedxuwa_user/views.py
@@ -3,12 +3,12 @@ from rest_framework.generics import ListAPIView
 from rest_framework import status
 from rest_framework.response import Response
 
-from .models import Member
+from .models import Member, CommitteeMember
 from .serializers import MemberSerializer, CommitteeMemberSerializer
 
 # Create your views here.
 
 
 class CommitteeListView(ListAPIView):
-    queryset = Member.objects.filter(committee__isnull=False)
+    queryset = CommitteeMember.objects.all()
     serializer_class = CommitteeMemberSerializer
