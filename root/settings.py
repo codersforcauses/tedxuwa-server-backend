e50c04d1b0f8ee9da73444a95ec253d9ab724903
Anh <anhlevietexe@gmail.com>
2018-05-22 20:42:42 +0800

    set allowed host

--- root/settings.py
+++ root/settings.py
@@ -42,7 +42,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEPLOYMENT != "PRODUCTION"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "tedxuwa.com",
+    "localhost:8000"
+]
 
 
 # Application definition
