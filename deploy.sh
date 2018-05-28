3c607f1ac63451e360d97d9aea450b729d7d154c
Anh <anhlevietexe@gmail.com>
2018-05-28 14:51:28 +0800

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,38 @@
 # better if we have an auto deployment system like git hooks
 # but I haven't been able to set it up yet
 
-# THIS ASSUME YOU'RE IN THE /
+# THIS ASSUME YOU'RE IN /website
 
 # front end deployment
-cd ../front-end/
+echo -n PULLING NEW FRONT END CODE...\ 
+cd front-end/
 git checkout master
 git pull
+echo [OK]
+
+echo -n BUILDING FRON END CODE...\ 
 npm run build
+echo [OK]
+
 # back end deployment
+echo -n PULLING NEW BACK END CODE...\ 
 cd ../back-end/
 git checkout master
 git pull
+echo [OK]
+
+echo -n INSTALLING REQUIREMENTS...\ 
 source env/bin/activate
 pip install -r requirements.txt
-python manage.py collectstatic --noinput  # collect front end
+echo [OK]
+
+echo -n COLLECTING STATIC FILES...\ 
+python manage.py collectstatic --noinput --clear
+echo [OK]
+
+echo -n RUNNING MIGRATIONS...\ 
 python manage.py migrate  # migrate the database
-python manage.py runserver 0.0.0.0:8000  # start the server on port 8000
+echo [OK]
+
+echo -n STARTING SERVER...\ 
+python manage.py runserver 0.0.0.0:80  # start the server on port 8000
