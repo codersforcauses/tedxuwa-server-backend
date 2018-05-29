b82bb603fbabd2e8c05f88f99b26dcb0d0240c81
Anh <anhlevietexe@gmail.com>
2018-05-29 13:12:14 +0800

    Revert "revert changes"
    This reverts commit 71abfff3cba07d76a37324d1820d7ccd3081ff04.

--- deploy.sh
+++ deploy.sh
@@ -2,51 +2,38 @@
 # This is a manual deployment script for ease of life. Would be much
 # better if we have an auto deployment system like git hooks
 # but I haven't been able to set it up yet
-# a copy of this script should live at /website/
 
 # THIS ASSUME YOU'RE IN /website
 
 # front end deployment
-echo -n PULLING NEW FRONT END CODE...
-{
-    cd front-end/
-    git checkout master
-    git pull
-} &> /dev/null
+echo PULLING NEW FRONT END CODE...
+cd front-end/
+git checkout master
+git pull
 echo [OK]
 
-echo -n BUILDING FRONT END CODE...
-{
-    npm run build
-} &> /dev/null
+echo BUILDING FRONT END CODE...
+npm run build
 echo [OK]
 
 # back end deployment
-echo -n PULLING NEW BACK END CODE...
-{
-    cd ../back-end/
-    git checkout master
-    git pull
-} &> /dev/null
+echo PULLING NEW BACK END CODE...
+cd ../back-end/
+git checkout master
+git pull
 echo [OK]
 
-echo -n INSTALLING REQUIREMENTS...
-{
-    source env/bin/activate
-    pip install -r requirements.txt
-} &> /dev/null
+echo INSTALLING REQUIREMENTS...
+source env/bin/activate
+pip install -r requirements.txt
 echo [OK]
 
-echo -n COLLECTING STATIC FILES...
-{
-    python manage.py collectstatic --noinput --clear
-} &> /dev/null
+echo COLLECTING STATIC FILES...
+python manage.py collectstatic --noinput --clear
 echo [OK]
 
-echo -n RUNNING MIGRATIONS...
-{
-    python manage.py migrate  # migrate the database
-} &> /dev/null
+echo RUNNING MIGRATIONS...
+python manage.py migrate  # migrate the database
 echo [OK]
 
 echo STARTING SERVER...
