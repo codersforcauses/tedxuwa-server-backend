0ea248a20e7d2eaa1661e969423c93e16056b7fc
Anh <anhlevietexe@gmail.com>
2018-05-29 13:09:08 +0800

    cool printing for deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,38 +2,51 @@
 # This is a manual deployment script for ease of life. Would be much
 # better if we have an auto deployment system like git hooks
 # but I haven't been able to set it up yet
+# a copy of this script should live at /website/
 
 # THIS ASSUME YOU'RE IN /website
 
 # front end deployment
-echo PULLING NEW FRONT END CODE...
-cd front-end/
-git checkout master
-git pull
+echo -n PULLING NEW FRONT END CODE...
+{
+    cd front-end/
+    git checkout master
+    git pull
+} &> /dev/null
 echo [OK]
 
-echo BUILDING FRONT END CODE...
-npm run build
+echo -n BUILDING FRONT END CODE...
+{
+    npm run build
+} &> /dev/null
 echo [OK]
 
 # back end deployment
-echo PULLING NEW BACK END CODE...
-cd ../back-end/
-git checkout master
-git pull
+echo -n PULLING NEW BACK END CODE...
+{
+    cd ../back-end/
+    git checkout master
+    git pull
+} &> /dev/null
 echo [OK]
 
-echo INSTALLING REQUIREMENTS...
-source env/bin/activate
-pip install -r requirements.txt
+echo -n INSTALLING REQUIREMENTS...
+{
+    source env/bin/activate
+    pip install -r requirements.txt
+} &> /dev/null
 echo [OK]
 
-echo COLLECTING STATIC FILES...
-python manage.py collectstatic --noinput --clear
+echo -n COLLECTING STATIC FILES...
+{
+    python manage.py collectstatic --noinput --clear
+} &> /dev/null
 echo [OK]
 
-echo RUNNING MIGRATIONS...
-python manage.py migrate  # migrate the database
+echo -n RUNNING MIGRATIONS...
+{
+    python manage.py migrate  # migrate the database
+} &> /dev/null
 echo [OK]
 
 echo STARTING SERVER...
