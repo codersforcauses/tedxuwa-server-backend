cbf8e52e7c5d84be0c1c762535918e352ad0ddb1
Anh <anhvietle.info@gmail.com>
2018-10-28 18:48:10 +0800

    add sponsors to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Sponsor
 
 # Register your models here.
+
+
+@admin.register(Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+    model = Sponsor
+    search_fields = ("name",)
