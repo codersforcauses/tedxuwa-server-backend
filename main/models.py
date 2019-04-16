2188febb742ed61a6e51bfac31e10667b8d1f768
Anh <anhvietle.info@gmail.com>
2019-04-16 20:26:24 +0800

    add link and tier to sponsor model

--- main/models.py
+++ main/models.py
@@ -7,6 +7,9 @@ class Sponsor(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default="", blank=True)
     logo_image = models.URLField()
+    link = models.URLField(blank=True)
+    tier = models.IntegerField(default=3,
+                               help_text="3 is the lowest and 1 is the highest")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
--- main/serializers.py
+++ main/serializers.py
@@ -5,4 +5,5 @@ from .models import Sponsor
 class SponsorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sponsor
-        fields = ("id", "name", "description", "logo_image")
+        fields = ("id", "name", "description", "logo_image",
+                  "link", "tier")
