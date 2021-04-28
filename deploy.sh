9e0a9d3cd9a36d21830deb9dd27828d25d77af98
Anh <anhlevietexe@gmail.com>
2021-04-28 20:00:52 +1000

    use gunicorn instead of uwsgi

--- deploy.sh
+++ deploy.sh
@@ -54,6 +54,8 @@ python manage.py migrate  # migrate the database
 echo [OK]
 
 echo STARTING SERVER...
-uwsgi --socket :8001 --module root.wsgi --home env -b 32768 --post-buffering 1 --master --pidfile=/tmp/website.pid -m --stats :1717 --stats-http
+sudo systemctl restart nginx
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn
 
 # https://code.djangoproject.com/ticket/19615
